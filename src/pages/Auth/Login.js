0b8675df9392cb83b949e152b6646ba14fb14765
Sannyk-kell <vasyok00ul@gmail.com>
2023-08-10 18:56:26 +0300

    PublicPrivateRoutes

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import Layout from '../../components/Layout/Layout'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation  } from 'react-router-dom'
 import toast from 'react-hot-toast'
 import "../../styles/AuthStyles.css"
 import { useAuth } from '../../context/auth'
@@ -12,6 +12,7 @@ function Login() {
     const [auth, setAuth] = useAuth()
 
     const navigate = useNavigate()
+    const location = useLocation()
 
     //form function
     const handleSubmit = async (e) => {
@@ -28,7 +29,7 @@ function Login() {
                     token: res.data.token,
                 })
                 localStorage.setItem('auth', JSON.stringify(res.data))
-                navigate('/')
+                navigate(location.state || '/')
             } else {
                 toast.error(res.data.message)
             }
