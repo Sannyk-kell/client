10745680267d6d049c4254297fd77ea78eceb15a
Sannyk-kell <vasyok00ul@gmail.com>
2023-08-16 15:53:09 +0300

    ForgotPassword

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -50,6 +50,9 @@ function Login() {
                 <div className="mb-3">
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder='Enter Your Password' required/>
                 </div>
+                <div className='mb-3'>
+                    <button type="submit" className="btn btn-primary" onClick={() => {navigate('/forgot-password')}}>Forgot Password</button>
+                </div>
                 <button type="submit" className="btn btn-primary">LOGIN</button>
             </form>
         </div>
--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -11,6 +11,7 @@ const Register = () => {
     const [password, setPassword] = useState("")
     const [phone, setPhone] = useState("")
     const [address, setAddress] = useState("")
+    const [answer, setAnswer] = useState("")
     const navigate = useNavigate()
 
     //form function
@@ -18,7 +19,7 @@ const Register = () => {
         e.preventDefault()
         try {
             const res = await axios.post(
-                '/api/v1/auth/register', {name, email, password, phone, address}
+                '/api/v1/auth/register', {name, email, password, phone, address, answer}
             )
             if(res && res.data.success) {
                 toast.success(res.data && res.data.message)
@@ -53,6 +54,9 @@ const Register = () => {
                 </div>
                 <div className="mb-3">
                     <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='Enter Your Address' required/>
+                </div>
+                <div className="mb-3">
+                    <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='What is your favorite animal' required/>
                 </div>
                 <button type="submit" className="btn btn-primary">REGISTER</button>
             </form>
